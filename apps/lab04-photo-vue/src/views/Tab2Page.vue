764084445e646d295e67b3cdab60197ab21c720a
adithep2422 <adithep.ma@kkumail.com>
2026-01-11 22:53:47 +0700

    add usePhotoGallery method

--- apps/lab04-photo-vue/src/views/Tab2Page.vue
+++ apps/lab04-photo-vue/src/views/Tab2Page.vue
@@ -13,25 +13,25 @@
         </ion-toolbar>
       </ion-header>
 
-      <!-- CHANGE: Remove or comment out <ExploreContainer /> -->
-      <!-- <ExploreContainer name="Tab 2 page" /> -->
     </ion-content>
 
-    <!-- CHANGE: Add the floating action button -->
-      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
-        <ion-fab-button>
+    <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+        <!-- CHANGE: Add a click event listener to the floating action button. -->
+        <ion-fab-button @click="addNewToGallery()">
           <ion-icon :icon="camera"></ion-icon>
         </ion-fab-button>
       </ion-fab>
-      
+
   </ion-page>
 </template>
 
 <script setup lang="ts">
-// CHANGE: Add import from `ionicons/icons`
 import { camera } from 'ionicons/icons';
-// CHANGE: Update import from `@ionic/vue` to include necessary Ionic components
 import { IonPage, IonHeader, IonFab, IonFabButton, IonIcon, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-// CHANGE: Remove or comment out the ExploreContainer import
-// import ExploreContainer from '@/components/ExploreContainer.vue';
+
+// CHANGE: Add `usePhotoGallery` import
+import { usePhotoGallery } from '@/composables/usePhotoGallery';
+
+// CHANGE: Destructure `addNewToGallery` from `usePhotoGallery()
+const { addNewToGallery } = usePhotoGallery();
 </script>
