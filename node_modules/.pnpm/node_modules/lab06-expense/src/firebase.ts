ea83c4a8f0dd39e9b5f64b55ea4ef635a90f57b0
adithep2422 <adithep.ma@kkumail.com>
2026-02-10 01:50:51 +0700

    delete api key

--- node_modules/.pnpm/node_modules/lab06-expense/src/firebase.ts
+++ node_modules/.pnpm/node_modules/lab06-expense/src/firebase.ts
@@ -2,9 +2,9 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD14IAFZYdaSp4Y4vOHn5nCk3LATjo9JTo",
-  authDomain: "ionic-expense-916b6.firebaseapp.com",
-  projectId: "ionic-expense-916b6"
+  apiKey: "",
+  authDomain: "",
+  projectId: ""
 };
 
 const app = initializeApp(firebaseConfig);
